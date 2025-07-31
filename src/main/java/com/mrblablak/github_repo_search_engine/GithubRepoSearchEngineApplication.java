9314592beb79c5891a7eb26d11a4423769539149
mromak <michal.romak@cyberskiller.com>
2025-07-31 08:40:29 +0200

    spring initializr: spring boot 3.5

---
+++ src/main/java/com/mrblablak/github_repo_search_engine/GithubRepoSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.mrblablak.github_repo_search_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepoSearchEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrblablak/github_repo_search_engine/GithubRepoSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrblablak.github_repo_search_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepoSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
